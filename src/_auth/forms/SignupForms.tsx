827ca83faebffd14590e34777ab00f5608657aeb
MdRiabi <mhdriabi2013@gmail.com>
2024-04-28 13:57:12 +0200

    fin=xing the dark theme of tailwind

--- src/_auth/forms/SignupForms.tsx
+++ src/_auth/forms/SignupForms.tsx
@@ -1,7 +1,6 @@
-
-import { z } from "zod"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { Button } from "@/components/ui/button"
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -10,38 +9,35 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 
-
-import { useForm } from "react-hook-form"
-import {SignupValidation} from "@/lib/validation";
-
-
-
-
+import { useForm } from "react-hook-form";
+import { SignupValidation } from "@/lib/validation";
 
 const SignupForms = () => {
- 
-
   const form = useForm<z.infer<typeof SignupValidation>>({
     resolver: zodResolver(SignupValidation),
     defaultValues: {
-      name:"",
+      name: "",
       username: "",
-      email:"",
-      password: "" ,
+      email: "",
+      password: "",
     },
-  })
- 
+  });
+
   // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof SignupValidation>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
-    console.log(values)
+    console.log(values);
   }
   return (
     <Form {...form}>
+    <div className="sm:w-420 flex-center flex-col">
+      <img  src="/assets/images/logo.svg"/>
+
+    </div>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
         <FormField
           control={form.control}
@@ -62,7 +58,7 @@ const SignupForms = () => {
         <Button type="submit">Submit</Button>
       </form>
     </Form>
-  )
-}
+  );
+};
 
-export default SignupForms
+export default SignupForms;
